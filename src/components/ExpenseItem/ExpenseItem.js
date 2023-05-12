91fd37a78233da296bc9241e5db245411167c74b
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-05-12 18:51:40 +0300

    update ExpenseItem title on click using useState()

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,4 +1,8 @@
-import React from 'react';
+// useState is a React hook
+//useState always returns an array with 2 values
+//where the first value is the value(variable) itself
+//and the second value is the updating function that sets the new value
+import React, {useState} from 'react';
 import "./ExpenseItem.css"
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../UI/Card/Card';
@@ -7,13 +11,23 @@ const ExpenseItem = (props) => {
 	//props is an object that stores the data that it's passed to the component from another one 
 	// it makes the component reusable
 
+	// we are using array desctructuring to store both elements from useState in separate variables or constants as above
+	const [title, setTitle] = useState(props.expenseTitle);
+	
+	const clickHandler = () => {
+		setTitle("Updated !");
+		console.log(title);
+		
+	}
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate expenseFullDate={props.expenseDate} ></ExpenseDate> 
 			<div className="expense-item__description">
-				<h2>{props.expenseTitle}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">{props.expenseAmount}</div>
 			</div>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 }
