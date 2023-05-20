265278b85e54209ab16d9a7648e676748952ff7c
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-05-20 23:28:24 +0300

    improve logic for returning the content

--- src/components/ListExpenses/ExpensesList/ExpensesList.js
+++ src/components/ListExpenses/ExpensesList/ExpensesList.js
@@ -3,30 +3,30 @@ import './ExpensesList.css';
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
 
 const ExpensesList = (props) => {
-    
-    let expensesContent = <div className='expenses-list__fallback'><p>No expenses found.</p></div>;
-
     const filteredExpensesData = props.expensesData.filter(expense => {
 		return (new Date(expense.date).getFullYear().toString() === props.filteredYear)
 	});
 
 
-	if(filteredExpensesData.length > 0) {
-		/* render dinamically each expense using map()
-			map() will receive the array item as a parameter and a function that will be applied on each array element
-			*/
-		expensesContent = (filteredExpensesData.map(expense => 
-			<ExpenseItem
-				key={expense.id}
-				expenseDate={expense.date}
-				expenseTitle={expense.title}
-				expenseAmount={expense.amount}>
-			</ExpenseItem>
-			)
-		);
+	if(filteredExpensesData.length === 0) {
+		return <h2 className="expenses-list__fallback">No expenses found.</h2>
 	}
 
-    return <ul className="expenses-list">{expensesContent}</ul>;
+    /* render dinamically each expense using map()
+        map() will receive the array item as a parameter and a function that will be applied on each array element
+        */
+    return (
+        <ul className="expenses-list">
+            {filteredExpensesData.map(expense => 
+            <ExpenseItem
+                key={expense.id}
+                expenseDate={expense.date}
+                expenseTitle={expense.title}
+                expenseAmount={expense.amount}>
+            </ExpenseItem>
+            )}
+        </ul>
+    );
 };
 
 export default ExpensesList;
